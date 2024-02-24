75e1f8404de9663152946a74a47a7f3a241fff34
AlekseyLAS1912 <159919281+AlekseyLAS1912@users.noreply.github.com>
2024-02-24 19:57:20 +1200

    Update M5DZ1.py

--- M5DZ1.py
+++ M5DZ1.py
@@ -2,9 +2,10 @@ class House:
 
     def __init__(self):
         self.numberOfFloors = 10
-        for i in range(11):
-            print('Текущий этаж равен', i)
 
 house = House()
+
+for i in range(house.numberOfFloor):
+    print('Текущий этаж равен', i)
 
 
