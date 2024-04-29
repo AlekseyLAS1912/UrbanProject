9e3dc1996c89aab1351317ae82750b92dfb30c68
AlekseyLAS1912 <159919281+AlekseyLAS1912@users.noreply.github.com>
2024-04-29 16:33:25 +1200

    Add files via upload

---
+++ M9DZ3_Iter.py
@@ -0,0 +1,15 @@
+class EvenNumbers:
+    def __init__(self, a, b):
+        self.start = a
+        self.end = b
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.start >= self.end:
+            raise StopIteration
+        else:
+            self.start += 2
+            return self.start
+en = EvenNumbers
+for i in en(0, 55):
+    print(i)
